a4df7ad47c208cb38925bbc368f85cbe38aca465
Sara Gajda <saragajda@Saras-Air.attlocal.net>
2019-11-11 16:10:13 -0600

    finished studio

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+      return response.json();
+        }).then(function(json){
+            let astronauts = '';
+            for(astronaut of json){
+                astronauts += 
+                `<div class="astronaut">
+                <div class="bio">
+                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                <ul>
+                    <li>Hours in Space: ${astronaut.hoursInSpace}</li>
+                    <li>Active: ${astronaut.active}</li>
+                    <li>Skills: ${astronaut.skills}</li>
+                </ul>
+                </div>
+                <img class="avatar" src=${astronaut.picture}>
+                </div>`;
+            }
+            let container = document.getElementById("container");
+            container.innerHTML = astronauts;
+    });
+  });
